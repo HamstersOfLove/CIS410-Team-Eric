a1901aa78aeacaf730fc84a9fb45bf4818933c32
Josh Yang <Jpy@uoregon.edu>
2016-04-22 16:01:27 -0700

    First commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+
+	private Animator animator;
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D> ();
+		animator = this.GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		var horizontal = Input.GetAxis("Horizontal");
+		var vertical = Input.GetAxis ("Vertical");
+
+		Vector2 movement = new Vector2 (horizontal, vertical);
+
+		rb.AddForce (movement * speed);
+
+		if (horizontal > 0)
+		{
+			animator.SetInteger("Direction", 0);
+
+		}
+		else if (horizontal < 0)
+		{
+			animator.SetInteger("Direction", 1);
+		}
+	}
+}
