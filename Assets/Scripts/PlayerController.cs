8e4f3c817333210cc34f5f744892eb33b53e411a
Josh Yang <Jpy@uoregon.edu>
2016-04-27 10:35:37 -0700

    Multi jump still not fixed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,7 +10,7 @@ public class PlayerController : MonoBehaviour
 	private Transform button;
 	private Animator animator;
 	private Rigidbody2D rb;
-	private bool isGrounded;
+	private bool isGrounded = false;
 
 	// Use this for initialization
 	void Start()
@@ -23,48 +23,46 @@ public class PlayerController : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		
-		var horizontal = Input.GetAxis("Horizontal");
-
-		Vector2 movement = new Vector2 (horizontal, 0);
-
 		//Player Movement
-
-		//Left & Right
+		//Left/Right
 		if (Input.GetKey (KeyCode.RightArrow)) {
-			rb.velocity = movement * playerSpeed;
+			transform.Translate(Vector2.right * playerSpeed);
 
 			animator.SetInteger ("Direction", 0);
 			animator.speed = 1;
 
 
 		} else if (Input.GetKey (KeyCode.LeftArrow)) {
-			rb.velocity = movement * playerSpeed;
+			transform.Translate(Vector2.left * playerSpeed);
 
 			animator.SetInteger ("Direction", 1);
 			animator.speed = 1;
 
 		} else { 
-			//rb.velocity = UnityEngine.Vector2.zero;
 			animator.speed = 0;
 
-		}
+		} 
 
 		//Jump
-		if ((Input.GetMouseButtonDown (0) || Input.GetKeyDown (KeyCode.Space)) && isGrounded) {  
-
-			//button.GetComponent<Button> ().interactable = false;	
-
-			rb.AddForce (jumpHeight, ForceMode2D.Impulse);
-			//isGrounded = false;
+		if (Input.GetKeyDown (KeyCode.Space)) {  
+			if(isGrounded){
+				rb.AddForce (jumpHeight, ForceMode2D.Impulse);
+			}
 		}
 			
 
 	}
 
+	// Possible solution to multiple jumps in air... Not working currently.
 	void OnTriggerEnter(Collider other){
 		if (other.gameObject.CompareTag ("Ground")) {
 			isGrounded = true;
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if (other.CompareTag ("Ground")) {
+			isGrounded = false;
 		}
 	}
 
