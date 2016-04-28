d4565c3d09775da49a80f61ed80eb06130668c44
Josh Yang <Jpy@uoregon.edu>
2016-04-27 17:12:43 -0700

    Fixed sorting layers

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -23,32 +23,29 @@ public class PlayerController : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		//Player Movement
+		//----------------- Player Movement -----------------
 		//Left/Right
-
-
-
-		if (Input.GetKey (KeyCode.RightArrow)) {
+		if (Input.GetKey (KeyCode.RightArrow)) { // -------------------------  Right Movement ---
 			transform.Translate(Vector2.right * playerSpeed);
 
 			animator.SetInteger ("Direction", 0);
 			animator.speed = 1;
 
 
-		} else if (Input.GetKey (KeyCode.LeftArrow)) {
+		} else if (Input.GetKey (KeyCode.LeftArrow)) { // -------------------  Left Movement ----
 			transform.Translate(Vector2.left * playerSpeed);
 
 			animator.SetInteger ("Direction", 1);
 			animator.speed = 1;
 
-		} else { 
+		} else { // ------------------------------- Stops animation if no key is being held down ---
 			animator.speed = 0;
 
 		} 
 
 		//Jump
 		if (Input.GetKeyDown (KeyCode.Space)) {
-			if (isJumping == false) {
+			if (isJumping == false) { // --------------------- Checks for double jumps ---------
 				rb.AddForce (jumpHeight, ForceMode2D.Impulse);
 				isJumping = true;
 			}
@@ -56,12 +53,15 @@ public class PlayerController : MonoBehaviour
 		}
 	
 	}
-
+	// Player collision controls
 	void OnCollisionEnter2D(Collision2D col){
+
+		// Tests for collision with Ground tagged objects
 		if (col.gameObject.tag == "Ground") {
 			isJumping = false;
 		}
 
+		// Tests for collision with Enemy tagged objects
 		if (col.gameObject.tag == "Enemy") {
 			animator.SetInteger ("Direction", 2);
 
