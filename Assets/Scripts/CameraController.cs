e18212d069edf66c4e1e439a6de62666cfaa6b61
Matt Rossetto <mrr@uoregon.edu>
2016-05-09 15:33:41 -0700

    Camera position when player leaves bounding box

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,15 +5,23 @@ public class CameraController : MonoBehaviour {
 
 	public GameObject player;
 
+	private float height;
 	private Vector3 offset;
+	Camera cam = Camera.main;
+
 
 	// Use this for initialization
 	void Start () {
 		offset = transform.position - player.transform.position;
+		height = this.gameObject.transform.position.y;
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
-		this.gameObject.transform.position = new Vector3(player.gameObject.transform.position.x,this.gameObject.transform.position.y,this.gameObject.transform.position.z);
+		if (player.gameObject.transform.position.y > height) {
+			this.gameObject.transform.position = new Vector3 (player.gameObject.transform.position.x, player.gameObject.transform.position.y, this.gameObject.transform.position.z);
+		} else {
+			this.gameObject.transform.position = new Vector3 (player.gameObject.transform.position.x, height, this.gameObject.transform.position.z);
+		}
 	}
 }
