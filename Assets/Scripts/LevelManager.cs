356d4a77d7dba0a6d3e1fe87a887272a6b344efe
Matt Rossetto <mrr@uoregon.edu>
2016-05-11 00:32:40 -0700

    Gameover transition fix

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -3,6 +3,24 @@ using System.Collections;
 
 public class LevelManager : MonoBehaviour {
 
+	public string prevLevel;
+
+	void Awake() {
+		DontDestroyOnLoad(transform.gameObject);
+	}
+	 
+	// Use this for initialization
+	void Start () {
+		prevLevel = PlayerController.currentScene;
+
+	}
+
+	public void LoadLast()
+	{
+		Application.LoadLevel (prevLevel);
+	}
+
+
 	public void LoadScene(string name) {
 		Application.LoadLevel (name);
 	}
